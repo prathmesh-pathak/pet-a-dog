9eb85c0957ad4eaa74cd999e357adb54df736cc7
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-03-27 23:11:04 -0400

    Booking module added with email notification

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+
+const users = [];
+const dog = [];
+const booking = [];
+var loginFlag = false;
+var userEmail = '';
 
 const initializePassport = require('../passport-config');
 initializePassport(
@@ -10,10 +22,6 @@ initializePassport(
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
 );
-
-const users = [];
-const dog = [];
-var loginFlag = false;
 
 router.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs', { name: req.user.firstName });
@@ -95,7 +103,7 @@ router.get('/search-sitter/:name', checkAuthenticated, (req, res) => {
     });
 });
 
-router.get('/search-sitter/:name/contact/', checkAuthenticated, (req, res) => {
+router.get('/search-sitter/:name/contact', checkAuthenticated, (req, res) => {
     fs.readFile('sitter_list.json', (err, data) => {
         if (err) console.log(err);
         let sitter = JSON.parse(data);
@@ -109,6 +117,58 @@ router.get('/search-sitter/:name/contact/', checkAuthenticated, (req, res) => {
                 });
             }
         }
+    });
+});
+
+router.post('/search-sitter/:name/contact', checkAuthenticated, (req, res) => {
+    const today = new Date();
+    booking.push({
+        id: Date.now().toString(),
+        userFirstName: req.body.firstName,
+        userLastName: req.body.lastName,
+        sitterName: req.params.name,
+        bookingDate: today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate(),
+        serviceSelected: req.body.selectedService,
+        dropOffDate: req.body.dropOff,
+        dropOffTimeFrom: req.body.selectDropTimeFrom,
+        dropOffTimeTo: req.body.selectDropTimeTo,
+        pickUpDate: req.body.pickUp,
+        pickUpTimeFrom: req.body.selectPickTimeFrom,
+        pickUpTimeTo: req.body.selectPickTimeTo,
+        userEmail: req.body.userEmail,
+        pets: dog,
+        message: req.body.message
+    });
+    userEmail = req.body.userEmail;
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'petadogapp@gmail.com',
+            pass: 'cSPROJECT#1'
+        }
+    });
+    const data = ejs.renderFile(__dirname + '\\order-details.ejs', { bookingDetails: booking, dogData: booking.pets }, (err, data) => {
+        let mailOtions = {
+            from: 'petadogapp@gmail.com',
+            to: userEmail,
+            subject: 'Booking confirmation from Pet a Dog',
+            html: data
+        }
+        transporter.sendMail(mailOtions, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log("Email Sent");
+                res.redirect('/search-sitter/:name/booking-details');
+            }
+        });
+    });
+});
+
+router.get('/search-sitter/:name/booking-details', checkAuthenticated, (req, res) => {
+    res.render('booking-confirmed.ejs', {
+        bookingDetails: booking
     });
 });
 
@@ -153,7 +213,6 @@ router.post('/profile/add', checkAuthenticated, (req, res) => {
         children: req.body.children,
         isTrained: req.body.trained
     });
-    console.log(dog);
     res.redirect('/profile');
 });
 
